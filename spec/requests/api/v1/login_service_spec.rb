4ee1fe2117c2e0e1ba11ea09f487e77451a5a84e
Joshua O'bannon <jtobannon@gmail.com>
2020-03-02 23:22:45 -0700

    Login Service Test

--- spec/requests/api/v1/login_service_spec.rb
+++ spec/requests/api/v1/login_service_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "as a user, when I request a login" do 
   it "logs me in" do 
-    user = User.create!(email: "jtobannon@gmail.com", password_digest: "password")
+    user = User.create!(email: "jtobannon@gmail.com", password: "password")
     
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    post 'api/v1/sessions', { email: 'josh@sweater_weather.com', password: 'password' } 
+    params = {
+      email: 'josh@sweater_weather.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    post '/api/v1/sessions', params: params 
   end
 end
