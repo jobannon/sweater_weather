31c696f2c54aae996248b3cafbb26f28f27afd78
Joshua O'bannon <jtobannon@gmail.com>
2020-03-03 18:46:30 -0700

    Refactor Munchies

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,19 +1,6 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-    lat = LocationGetterJson.new(location_params).get_lat
-    lng = LocationGetterJson.new(location_params).get_lng
-
-    travel_time_english = LocationGetterJson.new(location_params, 'directions').get_travel_time
-    travel_time_seconds = LocationGetterJson.new(location_params, 'directions').get_travel_time_seconds
-    
-    time_adjust = Time.now.to_i + travel_time_seconds
-    json_forecast = ForecastGetterJson.new(lat,lng).get_forcast_future(time_adjust)
-
-    yelp_open_business = YelpGetterJson.new.get_businesses_with_coordinates(lat,lng, location_params[:food], time_adjust) 
-    yelp_obj = YelpGetter.new(yelp_open_business)
-
-    final_results_hash_builder = FinalResults.new(location_params, travel_time_english, json_forecast, yelp_obj)
-    render json: FinalResultsSerializer.new(final_results_hash_builder)
+    render json: MunchiesSerializer.new(Munchies.new(location_params))
   end
 
   private
--- spec/requests/api/v1/final_service_spec.rb
+++ spec/requests/api/v1/final_service_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "as a request" do 
-  xit "returns to me 
+  it "returns to me 
     -the end location
     -the travel time
     -the forecast at the end location
     -a restaurant at the location end that is chinese food" do  
+
+  allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
 
   get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
 
@@ -18,6 +20,5 @@ RSpec.describe "as a request" do
   expect(clean[:data][:attributes][:forecast]).to eq("Clear throughout the day.")
   expect(clean[:data][:attributes][:restaurant][:name]).to eq("Kan's Kitchen")
   expect(clean[:data][:attributes][:restaurant][:address]).to eq("1620 S Prairie Ave")
-
   end
 end
