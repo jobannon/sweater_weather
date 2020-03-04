9e3436a9c47959c43af57dedc0b528682b418dc3
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 01:05:14 -0700

    Add current user method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       render status: :ok, json: {api_token: user.api_token}
     else
       render status: :forbidden, json: {message: "Unauthorized - you entered the wrong password or email"}
