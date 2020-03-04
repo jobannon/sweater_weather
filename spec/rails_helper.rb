18423df0c7f7c15a9491ffebee94698a8cbbd4ae
Joshua O'bannon <jtobannon@gmail.com>
2020-03-03 21:36:05 -0700

    Setup VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,14 +11,14 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec' 
 
-# VCR.configure do |config|
-#   config.ignore_localhost = true
-#   config.cassette_library_dir = 'spec/fixtures/cassettes'
-#   config.hook_into :webmock
-#   config.configure_rspec_metadata!
-#   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
-#   config.filter_sensitive_data("<GITHUB_ACCESS_TOKEN>") { ENV['GITHUB_ACCESS_TOKEN'] }
-# end
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/fixtures/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<GITHUB_ACCESS_TOKEN>") { ENV['GITHUB_ACCESS_TOKEN'] }
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
