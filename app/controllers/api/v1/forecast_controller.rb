4d48c913f1c81e2304502df9f84a1c5f0e2b720c
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 01:05:01 -0700

    Add current user method

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,17 +1,16 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    lat = LocationGetterJson.new(location_params).get_lat
-    lng = LocationGetterJson.new(location_params).get_lng
-    json_forecast = ForecastGetterJson.new(lat,lng).get_forcast
-    
-    # parking_space_for_now
-    # left_something = ForecastLeftBox.new(json_forecast, location_params[:location])
-    # right_something = ForecastRightBox.new(json_forecast, location_params[:location])
-    # bottom_something = ForecastBottomBox.new(json_forecast, location_params[:location])
+    if current_user
+      lat = LocationGetterJson.new(location_params).get_lat
+      lng = LocationGetterJson.new(location_params).get_lng
+      json_forecast = ForecastGetterJson.new(lat,lng).get_forcast
 
-    whole_something = Forecast.new(json_forecast, location_params[:location])
+      whole_something = Forecast.new(json_forecast, location_params[:location])
 
-    render json: ForecastSerializer.new(whole_something)
+      render json: ForecastSerializer.new(whole_something)
+    else
+      render json: { error_message: "Not Authorized" }
+    end
   end
 
   private
