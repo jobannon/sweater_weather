b0a4292b3bca2f88bfa6bbfe01b84ceb65edb62a
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 01:07:27 -0700

    Update Munchies for google

--- app/poros/munchies.rb
+++ app/poros/munchies.rb
@@ -4,12 +4,12 @@ class Munchies
   def initialize(location_params)
     @id = nil
     @location_params = location_params
-    @lat = get_lat
-    @lng = get_lng
     @end_location = location_params[:end]
 
-    @travel_time = get_travel_time_english
-    @time_adjust = make_time_adjust
+    @google_data_obj = GoogleLocationMaker.new(location_params, 'directions')
+    @lat = @google_data_obj.lat
+    @lng = @google_data_obj.lng 
+    @travel_time = @google_data_obj.travel_time
 
     @forecast = get_forecast_json[:hourly][:summary]
 
@@ -20,28 +20,12 @@ class Munchies
 
   private 
 
-  def get_lat
-    LocationGetterJson.new(@location_params).get_lat
-  end
-
-  def get_lng
-    LocationGetterJson.new(@location_params).get_lng
-  end
-
-  def get_travel_time_english
-    LocationGetterJson.new(@location_params, 'directions').get_travel_time
-  end
-
-  def get_travel_time_seconds
-    LocationGetterJson.new(@location_params, 'directions').get_travel_time_seconds
-  end
-
   def get_forecast_json
-    ForecastGetterJson.new(@lat,@lng).get_forcast_future(@time_adjust)
+    ForecastGetterJson.new(@lat,@lng).get_forcast_future(@google_data_obj.time_adjust)
   end
 
   def get_yelp_json
-    YelpGetterJson.new.get_businesses_with_coordinates(@lat,@lng, @location_params[:food], @time_adjust)
+    YelpGetterJson.new.get_businesses_with_coordinates(@lat,@lng, @location_params[:food], @google_data_obj.time_adjust)
   end
 
   def make_yelp_data(yelp_open_business)
