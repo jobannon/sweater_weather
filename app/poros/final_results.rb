28368d8b863828803c2b3e669329375057afb0d7
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 01:06:43 -0700

    Comment out for now

--- app/poros/final_results.rb
+++ app/poros/final_results.rb
@@ -1,12 +1,12 @@
-class FinalResults
-  attr_reader :id, :end_location, :travel_time, :forecast, :restaurant
+# class FinalResults
+#   attr_reader :id, :end_location, :travel_time, :forecast, :restaurant
 
-  def initialize(location, travel_time, json_forecast, yelp_obj)
-    @end_location = location[:end]
-    @travel_time = travel_time
+#   def initialize(location, travel_time, json_forecast, yelp_obj)
+#     @end_location = location[:end]
+#     @travel_time = travel_time
 
-    @forecast = json_forecast[:hourly][:summary]
+#     @forecast = json_forecast[:hourly][:summary]
 
-    @restaurant = {name: yelp_obj.name, address: yelp_obj.address}
-  end
-end
+#     @restaurant = {name: yelp_obj.name, address: yelp_obj.address}
+#   end
+# end
