140fd7ec7e80c1b0805698b48860e1075f044db6
Joshua O'bannon <jtobannon@gmail.com>
2020-03-03 21:32:54 -0700

    Add sad path

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::RoadTripController < ApplicationController
   def show
-    find_user = User.find_by(road_trip_params[:api_key])
-    render json: RoadTripSerializer.new(RoadTrip.new)
+    current_user = User.find_by(api_token: location_params[:api_key])
+    if current_user.nil?
+      render status: :unauthorized, json: {message: "Please enter a valid API key"}
+    else
+      render status: :ok, json: RoadTripSerializer.new(RoadTrip.new(location_params))
+    end
   end
 
-  def road_trip_params
+  def location_params 
     params.permit(:origin, :destination, :api_key)
   end
 end 
