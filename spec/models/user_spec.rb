d7e7c7fb292763d673c3df58694da84c7da8d941
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 01:07:46 -0700

    Add user validation and testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :email } 
+  it { should validate_presence_of :password } 
+  it { should have_secure_password }
+  #can't get this to work...
+  xit { should have_secure_token User.api_token }
+  xit { should have_secure_token :api_token}
 end
