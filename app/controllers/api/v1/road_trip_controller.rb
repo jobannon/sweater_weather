1953035be46f29dd5fb4b2f39780b62e63716077
Joshua O'bannon <jtobannon@gmail.com>
2020-03-04 10:31:46 -0700

    update for Heroku

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
   def show
-    current_user = User.find_by(api_token: location_params[:api_key])
-    if current_user.nil?
+    this_current_user = User.find_by(api_token: location_params[:api_key])
+    this_current_user = User.where(api_token: location_params[:api_key])
+    if this_current_user.nil?
       render status: :unauthorized, json: {message: "Please enter a valid API key"}
     else
       render status: :ok, json: RoadTripSerializer.new(RoadTrip.new(location_params))
