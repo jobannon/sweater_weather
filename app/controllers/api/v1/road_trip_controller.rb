574761f5d000ed5e4e5f9a8d4d2b5ff557e812f3
Joshua O'bannon <jtobannon@gmail.com>
2020-03-10 20:39:22 -0600

    Update Readme

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::RoadTripController < ApplicationController
   def show
-    # this_current_user = User.find_by(api_token: location_params[:api_token])
-    # if this_current_user.nil?
-    #   render status: :unauthorized, json: {message: "Please enter a valid API key"}
-    # else
+    this_current_user = User.find_by(api_token: location_params[:api_token])
+    if this_current_user.nil?
+      render status: :unauthorized, json: {message: "Please enter a valid API key"}
+    else
       render status: :ok, json: RoadTripSerializer.new(RoadTrip.new(location_params))
-    # end
+    end
   end
 
   def location_params 
